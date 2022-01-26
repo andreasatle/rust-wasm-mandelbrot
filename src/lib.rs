04e620aa48585519bba0508429e867363bd59f7a
Andreas Atle <atle.andreas@gmail.com>
2022-01-26 14:27:53 -0600

    Added more docu to lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-//! snake_game is the engine of a Snake Game for the browser.
+//! mandelbrot is the engine of a Mandelbrot Image Generator for the browser.
+//! It is used with typescript to setup a minimal web-application, where it is
+//! possible to zoom in the image, which is automatically regenerated.
 
 /// Activate wasm_bindgen to be able to compile to wasm.
 use wasm_bindgen::prelude::*;
@@ -29,19 +31,40 @@ struct PointUsize {
     y: usize,
 }
 
-/// Contains all info about the Mandelbrot image.
+/// Contains all necessary info about the Mandelbrot image.
 #[wasm_bindgen]
 pub struct Mandelbrot {
+    /// Corner Point of image.
     z0: PointF64,
+
+    /// Number of Points in each direction.
     n: PointUsize,
+
+    /// Voxel size.
     d: PointF64,
+
+    /// Maximum number of iterations until Mandebrot Set is assumed.
     max_iter: usize,
+    
+    /// Number of colors in image.
     n_colors: usize,
+
+    /// Red RGB-value.
     red: u8,
+
+    /// Green RGB-value.
     green: u8,
+
+    /// Blue RGB-value.
     blue: u8,
-    img2: Vec<usize>,
+
+    /// Work vector of full image size.
+    work: Vec<usize>,
+
+    /// Image represented with u8.
     img: Vec<u8>,
+
+    /// Mapping from escape-iteration to interpolation-weight for the color.
     colormap: Vec<usize>,
 }
 
@@ -58,14 +81,30 @@ impl Mandelbrot {
             red,
             green,
             blue,
-            img2: Vec::with_capacity(nx*ny),
+            work: Vec::with_capacity(nx*ny),
             img: Vec::with_capacity(4*nx*ny),
             colormap: Vec::with_capacity(max_iter),
         };
         mandel
     }
 
-    pub fn rescale_problem(&mut self, rx0: f64, ry0: f64, dx: f64, dy: f64) {
+    /// Return the pointer to the image.
+    pub fn get_image(&self) -> *const u8 {
+        self.img.as_ptr()
+    }
+
+    /// Update image and compute a new image.
+    pub fn update_image(&mut self, x0: f64, y0: f64, dx: f64, dy: f64) {
+        self.rescale_problem(x0, y0, dx, dy);
+        self.count_iterations();
+        self.iteration_frequency();
+        self.frequency_cumsum();
+        self.iteration_binner();
+        self.iterations_to_color();
+    }
+
+    /// Rescale the box that constitutes the Mandelbrot image.
+    fn rescale_problem(&mut self, rx0: f64, ry0: f64, dx: f64, dy: f64) {
 
         let dxy = if dx.abs() > dy.abs() {dx} else {dy};
 
@@ -75,6 +114,7 @@ impl Mandelbrot {
         self.d.y = dxy*self.d.y;
     }
 
+    /// Get the coordinate for a multiple-index in the image.
     fn get_coord(&self, i: &PointUsize) -> PointF64 {
         PointF64 {
             x: self.z0.x+(i.x as f64+0.5)*self.d.x,
@@ -82,6 +122,8 @@ impl Mandelbrot {
         }
     }
 
+    /// Compute the escape iteration for one index.
+    /// 0 is returned when the maximum number of iterations are reached.
     fn count_iter_for_index(&self, i: &PointUsize) -> usize {
         let c = self.get_coord(&i);
         let mut z = PointF64{x:0.0, y:0.0};
@@ -99,15 +141,15 @@ impl Mandelbrot {
         0
     }
 
-    /// Count the #iterations.
+    /// Count the escape iterations for all indices in the image.
     fn count_iterations(&mut self) {
-        self.img2.clear();
+        self.work.clear();
         let mut i: PointUsize = PointUsize{x:0,y:0};
         for iy in 0..self.n.y {
             i.y = iy;
             for ix in 0..self.n.x {
                 i.x = ix;
-                self.img2.push(self.count_iter_for_index(&i));
+                self.work.push(self.count_iter_for_index(&i));
             }
         }
     }
@@ -119,38 +161,25 @@ impl Mandelbrot {
 
         for i in 0..self.n.x*self.n.y {
             let i4 = i << 2;
-            self.img[i4] = (self.red as usize*self.colormap[self.img2[i]]/self.n_colors) as u8;
-            self.img[i4+1] = (self.green as usize*self.colormap[self.img2[i]]/self.n_colors) as u8;
-            self.img[i4+2] = (self.blue as usize*self.colormap[self.img2[i]]/self.n_colors) as u8;
+            self.img[i4] = (self.red as usize*self.colormap[self.work[i]]/self.n_colors) as u8;
+            self.img[i4+1] = (self.green as usize*self.colormap[self.work[i]]/self.n_colors) as u8;
+            self.img[i4+2] = (self.blue as usize*self.colormap[self.work[i]]/self.n_colors) as u8;
         }
     }
-
-    /// Return the pointer to the image.
-    pub fn get_image(&self) -> *const u8 {
-        self.img.as_ptr()
-    }
-
-    /// update_image and prepare a new image.
-    pub fn update_image(&mut self, x0: f64, y0: f64, dx: f64, dy: f64) {
-        self.rescale_problem(x0, y0, dx, dy);
-        self.count_iterations();
-        self.iteration_frequency();
-        self.frequency_cumsum();
-        self.iteration_binner();
-        self.iterations_to_color();
-    }
     
+    /// Count the frequency (or occurance) of each escape iteration.
     fn iteration_frequency(&mut self) {
         // Initialize the array to zero.
         self.colormap.clear();
         self.colormap.resize(self.colormap.capacity(),0);
 
         // Count the frequency of the different iterations.
-        for i in 0..self.img2.len() {
-            self.colormap[self.img2[i]] += 1;
+        for i in 0..self.work.len() {
+            self.colormap[self.work[i]] += 1;
         }
     }
 
+    /// Take the cumulative sum, except for the first entry.
     fn frequency_cumsum(&mut self) {
         // Skip the count of the actual Mandelbrot Set.
         self.colormap[0] = 0;
