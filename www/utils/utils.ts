b2e06d55b201f3d244b3b45b0963c0b67fa90216
Andreas Atle <atle.andreas@gmail.com>
2022-01-26 19:19:13 -0600

    Add comments to utility functions.

--- www/utils/utils.ts
+++ www/utils/utils.ts
@@ -1,3 +1,7 @@
+// typescript utility functions
+// that can be used in rust-wasm.
+
+// Wrapper routine to write on the console in the browser.
 export function output_js(str: string) {
     console.log(str);
 }
